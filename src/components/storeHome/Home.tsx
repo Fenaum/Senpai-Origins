02158b469f9171b39fe0d8e69adb70522a03fb62
Raymond Nguyen <125716839+Fenaum@users.noreply.github.com>
2024-04-15 22:54:13 -0700

    fixed animation bug

--- src/components/storeHome/Home.tsx
+++ src/components/storeHome/Home.tsx
@@ -1,8 +1,22 @@
-// src/components/storeHome/Home.tsx
+import Product from "../../../model/Product";
 
-import React from "react";
+// // src/components/storeHome/Home.tsx
+// async function getProducts() {
+//   const response = await fetch("http://localhost:3000/api/products/get", {
+//     method: "GET",
+//   });
+//   const products = await response.json();
+
+//   return products;
+// }
 
 const Home = () => {
+  // const products = getProducts().then((data) => {
+  //   return data;
+  // });
+
+  // console.log(products);
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-4xl font-bold mb-4">
@@ -13,29 +27,17 @@ const Home = () => {
       <div className="grid grid-cols-3 gap-4 my-10">
         {/* Example product cards */}
         <div className="border border-gray-300 p-4">
-          <img
-            src="/path/to/image1.jpg"
-            alt="Product 1"
-            className="w-full h-48 object-cover"
-          />
+          <img src="" alt="Product 1" className="w-full h-48 object-cover" />
           <h2 className="text-xl font-bold mt-2">Product 1</h2>
           <p className="text-gray-600">$99.99</p>
         </div>
         <div className="border border-gray-300 p-4">
-          <img
-            src="/path/to/image2.jpg"
-            alt="Product 2"
-            className="w-full h-48 object-cover"
-          />
+          <img src="" alt="Product 2" className="w-full h-48 object-cover" />
           <h2 className="text-xl font-bold mt-2">Product 2</h2>
           <p className="text-gray-600">$199.99</p>
         </div>
         <div className="border border-gray-300 p-4">
-          <img
-            src="/path/to/image3.jpg"
-            alt="Product 3"
-            className="w-full h-48 object-cover"
-          />
+          <img src="" alt="Product 3" className="w-full h-48 object-cover" />
           <h2 className="text-xl font-bold mt-2">Product 3</h2>
           <p className="text-gray-600">$299.99</p>
         </div>
