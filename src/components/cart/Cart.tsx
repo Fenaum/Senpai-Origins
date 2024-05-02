8d306058c6dc8641a755ddbb58032d16b80f5397
Raymond Nguyen <125716839+Fenaum@users.noreply.github.com>
2024-05-01 20:54:31 -0700

    cart files added

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,9 +1,13 @@
 'use client'
+import { useState } from 'react'
 
 export default function Cart() {
     return (
         <div className="cart-container"> 
-            <h2>Cart</h2>
+           <h2>Cart</h2>
+           <div>
+               {/* CartItem.tsx */}
+           </div>
         </div>
     )
 }
